602e5dcbd2fdbc01707c62f42e1a6404d6bf4a76
Francisco Javier García Gutiérrez <javiergarcia.tech@gmail.com>
2017-08-19 03:06:00 -0500

    Floating Canvas Active

---
+++ Assets/Scripts/EventCollider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BoxCollider))]
+public class EventCollider : MonoBehaviour {
+
+
+	[SerializeField]
+	private string message;
+	[SerializeField]
+	private GameObject balloon;
+	[SerializeField]
+	private string tagName;
+	[SerializeField]
+	private float messageDuration;
+	[SerializeField]
+	private int times;
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag (tagName) && times <= 0) {
+			balloon.SetActive (true);
+			balloon.transform.GetChild (0).GetComponent<Text> ().text = message;
+			//StartCoroutine (CloseMessage( messageDuration) );
+		} else {
+			times--;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.CompareTag (tagName)) {
+			balloon.transform.GetChild (0).GetComponent<Text> ().text = "";
+			balloon.SetActive (false);
+		}
+	}
+
+	/*IEnumerator CloseMessage(float timeToWait){
+		yield return new WaitForSeconds (timeToWait);
+
+	}*/
+
+
+}
---
+++ Assets/Scripts/GrandpaController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrandpaController : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	[SerializeField]
+	private string message;
+	[SerializeField]
+	private GameObject balloon;
+	[SerializeField]
+	private float messageDuration;
+	[SerializeField]
+	private int times;
+
+	void Start () {
+		StartCoroutine (OpenMessage(messageDuration,times));
+	}
+
+	void Update () {
+		transform.LookAt (target);
+	}
+
+	IEnumerator OpenMessage(float timeToWait,int timesResidue){
+		if (timesResidue > 0) {
+			yield return new WaitForSeconds (timeToWait);
+			balloon.SetActive (true);
+			balloon.transform.GetChild (0).GetComponent<Text> ().text = message;
+			StartCoroutine (CloseMessage(messageDuration,timesResidue));
+		}
+	}
+	IEnumerator CloseMessage(float timeToWait,int timesResidue){
+		yield return new WaitForSeconds (timeToWait);
+		balloon.transform.GetChild (0).GetComponent<Text> ().text = "";
+		balloon.SetActive (false);
+		StartCoroutine (OpenMessage(messageDuration,timesResidue-1));
+	}
+
+}
