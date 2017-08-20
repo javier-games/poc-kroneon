e180a366ea65b9d2fb16e37c9fd715898ed11e2d
Francisco Javier García Gutiérrez <javiergarcia.tech@gmail.com>
2017-08-20 17:43:12 -0500

    Preparing Video

---
+++ Assets/Scripts/PrepareVideo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PrepareVideo : MonoBehaviour {
+
+	[SerializeField]
+	private VideoPlayer videoPlayer;
+
+	void Start () {
+		videoPlayer.Prepare ();
+		StartCoroutine( PlayVideo ());
+	}
+		IEnumerator PlayVideo(){
+		videoPlayer.Prepare();
+		Debug.Log ("no play");
+		while (!videoPlayer.isPrepared)
+			yield return null;
+		
+		videoPlayer.Play();
+		Debug.Log ("play");
+		while (videoPlayer.isPlaying)
+			yield return null;
+		transform.gameObject.SetActive (false);
+		Debug.Log ("stop");
+	}
+}
